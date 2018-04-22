80529904822cdb781d084c2c6913d90b81172e42
Gilson Vieira <gilson.vieira@clickbus.com>
2018-04-22 01:37:42 -0300

    Wait until download completes

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 import time
+import tempfile
+import input
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from credentials import *
-from input import *
 
-defaultDownloadPath = os.path.abspath(defaultDownloadPath)
+
+# TODO use a timeout mechanism
+def wait_file_exists(file_path):
+    while not os.path.exists(file_path):
+        time.sleep(0.1)
+
+
+defaultDownloadPath = tempfile.mkdtemp()
 
 profile = {
     "plugins.plugins_list": [
@@ -22,7 +30,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", profile)
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
-driver.get(portalURL)
+driver.get(input.portalURL)
 assert "Portal RH - Logon" in driver.title
 
 # LOGIN
@@ -50,15 +58,18 @@ typeSelect.select_by_visible_text("MENSAL")
 # Given search date
 dateElement = driver.find_element_by_id("controlsAscx111_txtDataRef")
 dateElement.clear()
-dateElement.send_keys(searchDate)
+dateElement.send_keys(input.searchDate)
 
 # search
 driver.find_element_by_id("controlsAscx111_btnDemoConsultar").click()
 
-time.sleep(3)
+download_file_path = os.path.join(defaultDownloadPath, 'Auto_PrincipalConteudo.aspx')
+pdf_file_path = os.path.abspath(input.defaultDownloadPath)
+
+wait_file_exists(download_file_path)
 
 # Rename
 os.rename(
-    os.path.join(defaultDownloadPath, "Auto_PrincipalConteudo.aspx"),
-    os.path.join(defaultDownloadPath, (searchDate.replace("/", "-") + ".pdf")),
+    os.path.join(download_file_path),
+    os.path.join(pdf_file_path, (input.searchDate.replace("/", "-") + ".pdf")),
 )
