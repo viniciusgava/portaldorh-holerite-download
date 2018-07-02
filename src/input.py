2e89d5cc02cce23a847b2a734a8067ecbdd24628
Vinicius Gava <gava.vinicius@gmail.com>
2018-07-01 23:53:11 -0300

    Add docker container support

---
+++ src/input.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+if os.environ.get('RH_SEARCHDATE') is None:
+    sys.exit("Missing ENV VARIABLE RH_USERNAME")
+
+searchDate = os.environ.get('RH_SEARCHDATE')
+
+# Default portal do RH URL, if it is not configured by env variable
+portalURL = os.environ.get(
+    "RH_URL",
+    "https://www.portaldorh.com.br/portal_rckt/Auto_Principal.aspx"
+)
+
+# Default download path, if it is not configured by env variable
+defaultDownloadPath = os.environ.get(
+    "DOWNLOAD_PATH",
+    "/usr/workspace/downloads"
+)
+
+# Ensure downloads path exists
+if os.path.exists(defaultDownloadPath) is False:
+    os.makedirs(defaultDownloadPath)
